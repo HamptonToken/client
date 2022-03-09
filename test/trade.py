b7e5b361a3f1304f573dba7a926c12d6a52fef3e
hampton <83888722+HamptonToken@users.noreply.github.com>
2022-03-09 14:56:10 -0800

    Update trade.py

--- test/trade.py
+++ test/trade.py
@@ -7,7 +7,7 @@ import hashlib
 
 
 api_key = '*' #put here your public key
-secret_key = '' #put here your secret key
+secret_key = '*' #put here your secret key
 request = '/api/v4/trade-account/balance' #put here request path. For obtaining trading balance use: /api/v4/trade-account/balance
 baseUrl = 'https://whitebit.com' #domain without last slash. Do not use https://whitebit.com/
 #If the nonce is similar to or lower than the previous request number, you will receive the 'too many requests' error message
