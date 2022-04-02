89c3826bdefc94d81ea0b0be2bb3299bf20ed81a
hampton <83888722+HamptonToken@users.noreply.github.com>
2022-04-02 08:47:12 -0700

    Update file.py

--- test/file.py
+++ test/file.py
@@ -1,1 +1,15 @@
 #
+# https://github.com/ipfs/py-ipfs-api.git
+#
+
+import ipfsapi
+
+api = ipfsapi.connect('127.0.0.1', 5001)
+
+hmeta_meta = api.add('hmeta.config')
+
+# {'Name': 'hmeta.config', 'Hash': 'QmWvgsuZkaWxN1iC7GDciEGsAqphmDyCsk3CVHh7XVUUHq', 'Size': '28'}
+
+#display
+api.cat('QmWvgsuZkaWxN1iC7GDciEGsAqphmDyCsk3CVHh7XVUUHq')
+
